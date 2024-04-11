96830e8d77a311070c19990254b52c7e358b8731
Tomas Garbovetzky <tomigarbo75@gmail.com>
2024-04-11 14:11:58 -0300

    Primer versión

---
+++ index.js
@@ -0,0 +1,24 @@
+let btnGolArg = document.getElementById('btnGolArg');
+let btnGolFra = document.getElementById('btnGolFra');
+let btnReset = document.getElementById('btnReset');
+
+let sumadorGolesArg = document.getElementById('sumadorArg');
+let sumadorGolesFra = document.getElementById('sumadorFra');
+let sumarGol = 1;
+
+//document.getElementById("sumadorArg").innerText = 0;
+
+btnGolArg.addEventListener("click", function () {
+    sumadorGolesArg.innerText = parseInt(sumadorGolesArg.innerText) + sumarGol;
+});
+
+btnGolFra.addEventListener("click", function () {
+    sumadorGolesFra.innerText = parseInt(sumadorGolesFra.innerText) + sumarGol;
+});
+
+btnReset.addEventListener("click", function () {
+    document.getElementById("sumadorArg").innerText = 0
+    document.getElementById("sumadorFra").innerText = 0
+});
+
+
